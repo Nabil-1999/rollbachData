9dba0304a6f27934fbaaf4fbf2d6ad7a6ef202eb
Nabil-1999 <hanbalnabil@gmail.com>
2022-09-28 23:34:42 +0100

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'rollbachData';
+  public name = 'Essa';
+  public editMode!: boolean;
+  oldName!: string;
+  startEdit() {
+    this.oldName = this.name;
+    this.editMode = !this.editMode;
+  }
+  save() {
+    this.editMode = !this.editMode;
+  }
+  cancel() {
+    this.editMode = !this.editMode;
+    this.name = this.oldName;
+  }
 }
